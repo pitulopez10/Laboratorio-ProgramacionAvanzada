1a4683170b95688ab91e40fe99e12defe2c33bc3
lupizzorno <luciavalentinapizzorno12@gmail.com>
2026-06-03 20:10:51 -0300

    Add attributes and methods to Venta class

--- logica/dominio/Venta.h
+++ logica/dominio/Venta.h
@@ -5,12 +5,27 @@
 #ifndef LABORATORIO3_VENTA_H
 #define LABORATORIO3_VENTA_H
 
+#include "DTFecha.h"
+#include "DTHora.h"
 
+#include <string>
+using namespace std;
 
 class Venta {
+    private:
+        string idVenta;
+        DTFecha fecha;
+        DTHora hora;
+        float precioTotal;
+    public:
+        Venta(string idVenta, DTFecha fecha, DTHora hora, float precioTotal);
+        string getIdVenta();
+        DTFecha getFecha();
+        DTHora getHora();
+        float getPrecioTotal();
 
+        void setFecha(DTFecha fecha);
+        void setHora(DTHora hora);
+        void setPrecioTotal(float precioTotal);
 };
-
-
-
 #endif //LABORATORIO3_VENTA_H
