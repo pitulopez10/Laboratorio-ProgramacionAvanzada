54ffccd7d3b18d58318273fcc808785237ad535b
lupizzorno <luciavalentinapizzorno12@gmail.com>
2026-06-03 20:11:58 -0300

    Implement Venta class with constructors and getters/setters

--- logica/dominio/Venta.cpp
+++ logica/dominio/Venta.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "Venta.h"
+#include <iostream>
+
+using namespace std;
+
+//CONSTRUCTORES
+Venta :: Venta(string idVenta, DTFecha fecha, DTHora hora, float precioTotal) {
+    this-> idVenta = idVenta;
+    this-> fecha = fecha;
+    this-> hora = hora;
+    this-> precioTotal = precioTotal;
+}
+
+//GETTERS
+
+string Venta :: getIdVenta() {
+    return idVenta;
+}
+
+DTFecha Venta :: getFecha() {
+    return fecha;
+}
+  
+DTHora Venta :: getHora() {
+    return hora;
+}
+ 
+float Venta :: getPrecioTotal() {
+    return precioTotal;
+}
+
+//SETTERS
+
+void Venta :: setFecha(DTFecha fecha) {
+    this-> fecha = fecha;
+}
+
+void Venta :: setHora(DTHora hora) {
+    this-> hora = hora;
+}
+      
+void Venta :: setPrecioTotal(float precioTotal) {
+    this-> precioTotal = precioTotal;
+}
