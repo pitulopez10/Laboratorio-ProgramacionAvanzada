62b964c51c7a7abc0b89cca807d91f41553a25b0
lupizzorno <luciavalentinapizzorno12@gmail.com>
2026-06-03 19:43:33 -0300

    Implement ClienteRegistrado class with getters and setters

--- logica/dominio/Cliente.cpp
+++ logica/dominio/Cliente.cpp
@@ -1,5 +1,37 @@
-//
-// Created by facuw on 3/6/2026.
-//
-
 #include "Cliente.h"
+
+using namespace std;
+
+ClienteRegistrado :: ClienteRegistrado(int rut, string nombreCompleto, string direccion, string correo) {
+    this-> rut = rut;
+    this-> nombreCompleto = nombreCompleto;
+    this-> direccion = direccion;
+    this-> correo = correo;
+}
+    
+int ClienteRegistrado::getRut() {
+    return rut;
+}
+
+string ClienteRegistrado::getNombreCompleto() {
+    return nombreCompleto;
+}
+
+string ClienteRegistrado::getDireccion() {
+    return direccion;
+}
+
+string ClienteRegistrado::getCorreo() {
+    return correo;
+}
+
+void ClienteRegistrado::setNombreCompleto(string nombreCompleto) {
+    this-> nombreCompleto = nombreCompleto;
+}
+
+void ClienteRegistrado::setDireccion(string direccion) {
+    this-> direccion = direccion;
+}
+void ClienteRegistrado::setCorreo(string correo) {
+    this-> correo = correo;
+}
