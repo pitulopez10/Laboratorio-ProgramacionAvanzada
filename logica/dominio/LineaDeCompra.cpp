6c52169ad1ad5a16719cd63d677243cf38affea9
lupizzorno <luciavalentinapizzorno12@gmail.com>
2026-06-03 19:59:05 -0300

    Implement LineaDeCompra class with getters and setters

--- logica/dominio/LineaDeCompra.cpp
+++ logica/dominio/LineaDeCompra.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "LineaDeCompra.h"
+//CONSTRUCTORES
+LineaDeCompra :: LineaDeCompra(float precioCompra) {
+    this-> precioCompra = precioCompra;
+}
+
+//GETTERS
+float LineaDeCompra :: getPrecioCompra() {
+    return precioCompra;
+}
+
+//SETTERS
+void LineaDeCompra :: setPrecioCompra(float precioCompra) {
+    this-> precioCompra = precioCompra;
+}
