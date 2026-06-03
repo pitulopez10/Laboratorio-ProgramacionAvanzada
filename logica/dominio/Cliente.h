3849cd2d8a6e6256aece45b5bb28a8ce4132fe7e
lupizzorno <luciavalentinapizzorno12@gmail.com>
2026-06-03 19:40:12 -0300

    Add ClienteRegistrado class with getters and setters

--- logica/dominio/Cliente.h
+++ logica/dominio/Cliente.h
@@ -1,16 +1,28 @@
-//
-// Created by facuw on 3/6/2026.
-//
-
 #ifndef LABORATORIO3_CLIENTE_H
 #define LABORATORIO3_CLIENTE_H
 
-
+#include <string>
+using namespace std;
 
 class Cliente {
-
 };
 
+class ClienteRegistrado : public Cliente {
+    private:
+        int rut;
+        string nombreCompleto, direccion, correo;
+    public:
+        ClienteRegistrado(int rut, string nombreCompleto, string direccion, string correo);
 
+        //GETTERS
+        int getRut();
+        string getNombreCompleto();
+        string getDireccion();
+        string getCorreo();
 
+        //SETTERS
+        void setNombreCompleto(string nombreCompleto);
+        void setDireccion(string direccion);
+        void setCorreo(string correo);
+};
 #endif //LABORATORIO3_CLIENTE_H
