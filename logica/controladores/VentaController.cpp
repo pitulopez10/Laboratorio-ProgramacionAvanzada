ee533945c21d7b4414ecdb89afe1a84d008cfa4f
lupizzorno <luciavalentinapizzorno12@gmail.com>
2026-06-09 17:47:47 -0300

    Add VentaController implementation with methods

--- logica/controladores/VentaController.cpp
+++ logica/controladores/VentaController.cpp
@@ -1,5 +1,23 @@
 //
 // Created by facuw on 3/6/2026.
 //
-
 #include "VentaController.h"
+
+#include <vector>
+#include <string>
+
+#include "../dominio/Venta.h"
+#include "../dominio/DTFecha.h"
+#include "../dominio/DTHora.h"
+
+using namespace std;
+
+VentaController::VentaController() {}
+
+void VentaController::nuevaVenta(string idVenta, DTFecha fecha, DTHora hora, float precioTotal) {}
+
+vector<Venta*> VentaController::listarVentas() {
+    return ventas;
+}
+
+void VentaController::consultarStock() {}
