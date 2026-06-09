c7e10b7dc9c3f3ce8690e888cc2f1bbf8a6fd289
lupizzorno <luciavalentinapizzorno12@gmail.com>
2026-06-09 18:44:04 -0300

    Implement main menu for user login options

--- vista/MenuPrincipal.cpp
+++ vista/MenuPrincipal.cpp
@@ -1,5 +1,49 @@
-//
-// Created by facuw on 3/6/2026.
-//
-
 #include "MenuPrincipal.h"
+#include <iostream>
+
+#include "MenuAdministrador.h"
+#include "MenuEmpleado.h"
+#include "MenuCliente.h"
+
+using namespace std;
+
+MenuPrincipal::MenuPrincipal(AdminController* adminCtrl,EmpleadoController* empleadoCtrl,VentaController* ventaCtrl)
+: menuAdmin(*adminCtrl),
+  menuEmpleado(empleadoCtrl),
+  menuCliente(ventaCtrl)
+  //el cliente realizar compras,consultar compras, calificar productos por eso se relaciona con ventactrl
+{
+}
+
+void MenuPrincipal :: mostrar() {
+    int opcion;
+
+    do {
+        cout<<"\n========================\n";
+        cout<<"Sistema Stock y Ventas\n";
+        cout<<"\n========================\n";
+
+        cout<<"1. Iniciar sesion como Administrador\n";
+        cout<<"2. Iniciar sesion como Empleado\n";
+        cout<<"3. Iniciar sesion como Cliente\n";
+        cout<<"4. Salir\n";
+        cout<<"Seleccione opcion:";
+        cin>>opcion;
+
+        switch (opcion) {
+            case 1:{
+                menuAdmin.mostrar();
+                break;}
+            case 2:{
+                menuEmpleado.mostrar();
+                break;}
+            case 3:{
+                menuCliente.mostrar();
+                break;}
+            case 4:{
+                cout<<"Saliendo...\n";
+                break;}
+            default: cout<<"Opcion invalida\n";
+        }
+    }while(opcion != 4);
+}
