0e209418fa0e28d1a6bf38089e311a4465977496
lupizzorno <luciavalentinapizzorno12@gmail.com>
2026-06-09 17:44:22 -0300

    Refactor VentaController header file structure

--- logica/controladores/VentaController.h
+++ logica/controladores/VentaController.h
@@ -1,16 +1,25 @@
-//
-// Created by facuw on 3/6/2026.
-//
-
 #ifndef LABORATORIO3_VENTACONTROLLER_H
 #define LABORATORIO3_VENTACONTROLLER_H
 
+#include <vector>
+#include <string>
 
+#include "../dominio/Venta.h"
+#include "../dominio/DTFecha.h"
+#include "../dominio/DTHora.h"
+
+using namespace std;
 
 class VentaController {
+private:
+    vector<Venta*> ventas;
 
+public:
+    VentaController();
+
+    void nuevaVenta(string idVenta, DTFecha fecha, DTHora hora, float precioTotal);
+    vector<Venta*> listarVentas();
+    void consultarStock();
 };
-
-
 
 #endif //LABORATORIO3_VENTACONTROLLER_H
