9169ab2e94676b5c9c2d41dd89bd0cda0bf2aa32
lupizzorno <luciavalentinapizzorno12@gmail.com>
2026-06-21 00:43:08 -0300

    empleado: altaCliente, modificarCliente

--- logica/dominio/Cliente.h
+++ logica/dominio/Cliente.h
@@ -13,24 +13,26 @@ class Cliente {
 class ClienteRegistrado : public Cliente {
     private:
         int rut;
-        string nombreCompleto, direccion, correo;
+        string nombreCompleto, direccion, correo, password;
 
         vector<Calificacion*> calificaciones;
 
     public:
-        ClienteRegistrado(int rut, string nombreCompleto, string direccion, string correo);
+        ClienteRegistrado(int rut, string nombreCompleto, string direccion, string correo, string password);
 
         //GETTERS
         int getRut() const;
         string getNombreCompleto() const;
         string getDireccion() const;
         string getCorreo() const;
+        string getPassword() const;
         vector<Calificacion*> getCalificaciones() const;
 
         //SETTERS
         void setNombreCompleto(string nombreCompleto);
         void setDireccion(string direccion);
         void setCorreo(string correo);
+        void setPassword(string password);
 
         //METODOS
         void agregarCalificacion(Calificacion* calificacion);
